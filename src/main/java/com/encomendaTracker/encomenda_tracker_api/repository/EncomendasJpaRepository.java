b5548e91c5dc224b237cbdadf7c0f4528b28123d
jjsales <jamilesales083@gmail.com>
2025-09-25 12:52:00 -0300

    [PORT-01] estrutura de pacotes

--- src/main/java/com/encomendaTracker/encomenda_tracker_api/repository/EncomendasJpaRepository.java
+++ src/main/java/com/encomendaTracker/encomenda_tracker_api/repository/EncomendasJpaRepository.java
@@ -3,5 +3,5 @@ package com.encomendaTracker.encomenda_tracker_api.repository;
 import com.encomendaTracker.encomenda_tracker_api.entities.EncomendasEntities;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EncomendasJpaRepository extends JpaRepository<EncomendasEntities<V>, Long> {
+public interface EncomendasJpaRepository<V> extends JpaRepository<EncomendasEntities<V>, Long> {
 }
