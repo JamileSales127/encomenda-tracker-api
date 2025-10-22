8ab8689355accbd657bba23e8ed1c42f19fbb52c
jamile <jamilesales083@gmail.com>
2025-10-21 22:09:09 -0300

    [PORT-02]criação de entidade

--- src/main/java/com/encomendaTracker/encomenda_tracker_api/entities/EncomendasEntities.java
+++ src/main/java/com/encomendaTracker/encomenda_tracker_api/entities/EncomendasEntities.java
@@ -1,7 +1,68 @@
 package com.encomendaTracker.encomenda_tracker_api.entities;
 
+import com.encomendaTracker.encomenda_tracker_api.enums.StatusEncomenda;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
 
 @Entity
 public class EncomendasEntities {
+    @Id
+    private Long id;
+
+    @NotNull
+    private String codigoRastreio;
+
+    private String descricao;
+
+    private String ultimoLocal;
+
+    private StatusEncomenda status;
+
+    private LocalDateTime dataDeAtualizacao;
+
+
+    public EncomendasEntities() {
+
+    }
+
+    public EncomendasEntities(String codigoRastreio, String descricao, Long id, String ultimoLocal, StatusEncomenda status, LocalDateTime dataDeAtualizacao) {
+        this.codigoRastreio = codigoRastreio;
+        this.descricao = descricao;
+        this.id = id;
+        this.ultimoLocal = ultimoLocal;
+        this.status = status;
+        this.dataDeAtualizacao = dataDeAtualizacao;
+    }
+
+
+    public String getCodigoRastreio() {
+        return codigoRastreio;
+    }
+
+    public LocalDateTime getDataDeAtualizacao() {
+        return dataDeAtualizacao;
+    }
+
+    public StatusEncomenda getStatus() {
+        return status;
+    }
+
+    public String getUltimoLocal() {
+        return ultimoLocal;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
