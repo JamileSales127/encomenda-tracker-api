4ac2cf53d92de491c9911ab4f5d73550a3671d40
jamile <jamilesales083@gmail.com>
2025-10-21 22:12:59 -0300

    [PORT-02]criação de entidade

--- src/main/java/com/encomendaTracker/encomenda_tracker_api/vo/EncomendaVO.java
+++ src/main/java/com/encomendaTracker/encomenda_tracker_api/vo/EncomendaVO.java
@@ -9,18 +9,36 @@ public class EncomendaVO {
     private Long codigoRastreio;
     private StatusEncomenda status;
     private LocalDateTime dateUpdate;
-    private LocalDateTime datePrevistaEntrega;
+    private String descricao;
+    private String ultimoLocal;
 
-    public EncomendaVO(){
+    public EncomendaVO() {
 
     }
 
-    public EncomendaVO(int id, StatusEncomenda status, Long codigoRastreio, LocalDateTime dateUpdate, LocalDateTime datePrevistaEntrega) {
+    public EncomendaVO(int id, StatusEncomenda status, Long codigoRastreio, LocalDateTime dateUpdate,  String descricao, String ultimoLocal) {
         this.id = id;
         this.status = status;
         this.codigoRastreio = codigoRastreio;
         this.dateUpdate = dateUpdate;
-        this.datePrevistaEntrega = datePrevistaEntrega;
+        this.descricao = descricao;
+        this.ultimoLocal = ultimoLocal;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getUltimoLocal() {
+        return ultimoLocal;
+    }
+
+    public void setUltimoLocal(String ultimoLocal) {
+        this.ultimoLocal = ultimoLocal;
     }
 
     public int getId() {
@@ -55,14 +73,6 @@ public class EncomendaVO {
         this.status = status;
     }
 
-    public LocalDateTime getDatePrevistaEntrega() {
-        return datePrevistaEntrega;
-    }
-
-    public void setDatePrevistaEntrega(LocalDateTime datePrevistaEntrega) {
-        this.datePrevistaEntrega = datePrevistaEntrega;
-    }
-
     @Override
     public String toString() {
         return "EncomendaVO{" +
@@ -70,7 +80,6 @@ public class EncomendaVO {
                 ", codigoRastreio=" + codigoRastreio +
                 ", status=" + status +
                 ", dateUpdate=" + dateUpdate +
-                ", datePrevistaEntrega=" + datePrevistaEntrega +
                 '}';
     }
 }
